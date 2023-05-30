3858005553720dddd419bb9c0298e985f50f5341
Berk Emre Uzun <54277320+Truirer@users.noreply.github.com>
2023-05-31 02:23:50 +0300

    Added input and cleaned the code

--- src/index.js
+++ src/index.js
@@ -3,20 +3,11 @@ const fileInput = document.querySelector('input[type="file"');
 const asciiImage = document.getElementById('ascii');
 
 const context = canvas.getContext('2d');
+const video = document.getElementById('video');
 
 const toGrayScale = (r, g, b) => 0.21 * r + 0.72 * g + 0.07 * b;
 
-const getFontRatio = () => {
-    const pre = document.createElement('pre');
-    pre.style.display = 'inline';
-    pre.textContent = ' ';
-    document.body.appendChild(pre);
-    const { width, height } = pre.getBoundingClientRect();
-    document.body.removeChild(pre);
-    return height / width;
-};
 
-const fontRatio = getFontRatio();
 
 const convertToGrayScales = (context, width, height) => {
     const imageData = context.getImageData(0, 0, width, height);
@@ -39,42 +30,28 @@ const convertToGrayScales = (context, width, height) => {
     return grayScales;
 };
 
-const MAXIMUM_WIDTH = 80;
-const MAXIMUM_HEIGHT = 80;
-
-const clampDimensions = (width, height) => {
-    const rectifiedWidth = Math.floor(getFontRatio() * width);
-
-    if (height > MAXIMUM_HEIGHT) {
-        const reducedWidth = Math.floor(rectifiedWidth * MAXIMUM_HEIGHT / height);
-        return [reducedWidth, MAXIMUM_HEIGHT];
-    }
-
-    if (width > MAXIMUM_WIDTH) {
-        const reducedHeight = Math.floor(height * MAXIMUM_WIDTH / rectifiedWidth);
-        return [MAXIMUM_WIDTH, reducedHeight];
-    }
-
-    return [rectifiedWidth, height];
-};
 document.querySelector("button").addEventListener("click",(e)=>{
     e.target.innerHTML = video.paused ? "STOP":"PLAY" ;
     video.paused ? video.play():video.pause()
 })
 
 function renderAscii (e){
-        const [width, height] = clampDimensions(window.innerWidth*2,video.videoHeight);
+        const width = Math.floor(window.innerWidth/3);
+        const height = video.videoHeight/video.videoWidth*width;
 
         canvas.width = window.innerWidth;
-        canvas.height = video.videoHeight*2;
-        context.drawImage(e, 0, 0, width, height*2);
-        const grayScales = convertToGrayScales(context, width, height*2);
+        canvas.height = video.videoHeight;
+        context.drawImage(e, 0, 0, width, height);
+        const grayScales = convertToGrayScales(context, width, height);
         drawAscii(grayScales, width);
-
-
 };
 
-var video = document.getElementById('video');
+document.getElementById("input").addEventListener("change", function() {
+    var media = URL.createObjectURL(this.files[0]);
+    document.querySelector("button").innerHTML = "PLAY"
+    video.src = media;
+    video.pause()
+});
 // set canvas size = video size when known
 video.addEventListener('loadedmetadata', function() {
   canvas.width = video.videoWidth;
@@ -86,13 +63,12 @@ video.addEventListener('play', function() {
   (function loop() {
     if (!$this.paused && !$this.ended) {
       renderAscii($this)
-
       setTimeout(loop, (1000 / 60)); // drawing at 60fps
     }
   })();
 });
 
-const grayRamp = '$@0GCLft1i;:..,:;i1tfLCG0 ';
+const grayRamp = '$@0GCLft1i;:..,:;i1tfL ';
 const rampLength = grayRamp.length;
 
 const getCharacterForGrayScale = grayScale => grayRamp[Math.ceil((rampLength - 1) * grayScale / 255)];
